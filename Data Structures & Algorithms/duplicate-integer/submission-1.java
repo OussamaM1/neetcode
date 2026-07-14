2812873a9e1466b4146fee9018fe6cc8d831ca52
Makhlouk Oussama <57569309+OussamaM1@users.noreply.github.com>
2026-07-14 12:19:08 +0100

    Refactor hasDuplicate method to use streams

--- Data Structures & Algorithms/duplicate-integer/submission-1.java
+++ Data Structures & Algorithms/duplicate-integer/submission-1.java
@@ -1,10 +1,5 @@
 class Solution {
     public boolean hasDuplicate(int[] nums) {
-        // Set<Integer> set = new HashSet<>();
-        // for (int n : nums) {
-        //     set.add(n);
-        // }
-        // return set.size() != nums.length ? true : false;
         return Arrays.stream(nums).distinct().toArray().length != nums.length ? true : false;
     }
 }
